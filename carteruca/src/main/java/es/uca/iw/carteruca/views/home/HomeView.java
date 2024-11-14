29101c83d2d9a137c65576b985d9391b77338db6
celiagg99 <92980596+celiagg99@users.noreply.github.com>
2024-11-14 13:50:01 +0100

    HomeView - creacion

--- carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeView.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeView.java
@@ -1,4 +1,12 @@
 package es.uca.iw.carteruca.views.home;
 
-public class HomeView {
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class HomeView extends VerticalLayout {
+
+    public HomeView(){
+
+    }
 }
