b9b8ec62edd66741e8e56bb5affd95e1ba7bbde7
celiagg99 <92980596+celiagg99@users.noreply.github.com>
2024-11-14 16:20:32 +0100

    Create HomeRegistradoView.java

---
+++ carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeRegistradoView.java
@@ -0,0 +1,8 @@
+package es.uca.iw.carteruca.views.home;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class HomeRegistradoView extends Composite<VerticalLayout>{
+
+}
