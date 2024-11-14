dcba0402d6674d136a009911f573bf79c208c727
Danivangel <ivanguillen66@gmail.com>
2024-11-14 18:29:50 +0100

    Cambios y creacion de las clases centro y alineamiento

--- carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeView.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeView.java
@@ -1,8 +1,5 @@
 package es.uca.iw.carteruca.views.home;
 
-<<<<<<< Updated upstream
-public class HomeView {
-=======
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -12,5 +9,4 @@ public class HomeView extends VerticalLayout {
     public HomeView(){
 
     }
->>>>>>> Stashed changes
 }
