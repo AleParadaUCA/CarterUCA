991fa2545f0ff80c57a4f73bffdc7f47f21fe8e4
Danivangel <ivanguillen66@gmail.com>
2024-11-14 14:46:51 +0100

    Update usuario.java

--- carteruca/src/main/java/es/uca/iw/carteruca/models/usuario/usuario.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/models/usuario/usuario.java
@@ -1,60 +1,12 @@
 package es.uca.iw.carteruca.models.usuario;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table
-public class usuario {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @Column(length = 25,nullable = false)
-    private String username;
-
-    @Column(length = 25,nullable = false)
-    private String password;
-
-    @Column(length = 25,nullable = false)
-    private String name;
-
-    @Column(length = 50,nullable = false)
-    private String lastname;
-
-    @Column(length = 25,nullable = false)
-    private String email;
-
-    @Column
-    private String unidad;//Centro o unidad a la que pertenece el usuario
-    //puede que se tenga que poner en la solicitud y no en el usuario
-
-    @Column
-    private Rol rol;
-
-// Getters y setters
-
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
-
-    public String getUsername() {return username;}
-    public void setUsername(String username) {this.username = username;}
-
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
-
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
-
-    public String getLastname() {return lastname;}
-    public void setLastname(String lastname) {this.lastname = lastname;}
-
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-
-    public String getUnidad() {return unidad;}
-    public void setUnidad(String unidad) {this.unidad = unidad;}
-
-    public Rol getRol() {return rol;}
-    public void setRol(Rol rol) {this.rol = rol;}
+public record usuario(
+        Long id,
+        String username,
+        String password,
+        String name,
+        String lastname,
+        String email,
+        Rol rol
+        ) {
 }
