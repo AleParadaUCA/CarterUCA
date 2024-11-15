43befd3ae9b134361c6289c596b0bb6e9576ce93
Alejandro Parada <alejandro.paradaespinosa@alum.uca.es>
2024-11-16 00:56:49 +0100

    header barra de búsqueda

--- carteruca/src/main/java/es/uca/iw/carteruca/views/layout/Header.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/views/layout/Header.java
@@ -2,23 +2,37 @@ package es.uca.iw.carteruca.views.layout;
 
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-
 public class Header extends Composite<HorizontalLayout> {
 
     public Header() {
-        HorizontalLayout layout = getContent();
-        layout.setWidthFull();
-        layout.getStyle().setBackground("#384850")
-                        .setColor("white")
-                        .setPadding("10px");
+        HorizontalLayout header = getContent();
 
+        header.setWidthFull();
+        header.getStyle()
+                .setBackground("#384850")
+//                .setColor("white")
+                .setPadding("10px");
+
+        //Barra de Búsqueda
         TextField searchField = new TextField();
         searchField.setPlaceholder("Buscar...");
-        searchField.addClassName("text-field-white");
-        searchField.setWidth("400px");
+        searchField.setWidth("20%");
+        searchField.getStyle()
+                .set("--vaadin-input-field-placeholder-color", "white")
+                .setBackground("#455a64")
+                .set("color", "white");
 
-        layout.add(searchField);
+        // Añadir un ícono de lupa blanco
+        Icon searchIcon = VaadinIcon.SEARCH.create();
+        searchIcon.getStyle().set("color", "white");
+        searchField.setSuffixComponent(searchIcon);
+
+
+        header.add(searchField);
+        header.add(new Span("header"));
     }
 }
