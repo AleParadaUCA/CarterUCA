5fe84569041e3cc16e99a956de5319739d2cd0f1
Celia <92980596+celiagg99@users.noreply.github.com>
2024-11-21 21:45:49 +0100

    Vista de solicitud
    Añadidos botones, todavia queda trabajo para terminar la vista

--- carteruca/src/main/java/es/uca/iw/carteruca/views/solicitud/SolicitudAddView.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/views/solicitud/SolicitudAddView.java
@@ -1,18 +1,25 @@
 package es.uca.iw.carteruca.views.solicitud;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.charts.model.Tooltip;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.combobox.MultiSelectComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
 import es.uca.iw.carteruca.views.layout.MainLayout;
 import jakarta.annotation.security.RolesAllowed;
 
@@ -21,46 +28,66 @@ import jakarta.annotation.security.RolesAllowed;
 @RolesAllowed({"Promotor","CIO","Solicitante"})
 public class SolicitudAddView extends Composite<VerticalLayout> {
 
-    private H1 title = new H1();
-    private TextField titulo = new TextField();
-    private TextField intersesados = new TextField();
-    private TextField alimiamiento  = new TextField();
-    private TextField alcance = new TextField();
-    private TextField normativa = new TextField();
-    private MemoryBuffer memoria = new MemoryBuffer();
+    private final TextField titulo = new TextField();
+    private final TextField nombre = new TextField();   //nombre corto del proyecto
+    private final TextField interesados = new TextField();
+    private final TextField objetivos = new TextField();
+    private final TextField alcance = new TextField();
+    //private final MultiSelectComboBox<String> normativa = new MultiSelectComboBox<>("Normativa");  //multiple hay que disponerlo
+    private final ComboBox<String> normativa = new ComboBox<>();
+    private final DatePicker fecha_puesta = new DatePicker();
+    private final MemoryBuffer memoria = new MemoryBuffer();
+    private final ComboBox<String> avalador = new ComboBox<>();
 
     public SolicitudAddView() {
 
+        // Alineación del layout principal para centrar el contenido
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
-        getContent().setJustifyContentMode(FlexComponent.JustifyContentMode.START);
+        getContent().setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
         getContent().setAlignItems(FlexComponent.Alignment.CENTER);
 
-        title.setText("Agregar Solicitud");
+        // Layout principal
+        VerticalLayout principal = new VerticalLayout();
+        principal.setWidth("100%");
+        principal.setMaxWidth("800px"); // Limitar el ancho del formulario para mejorar su presentación
 
-        getContent().add(title);
+        // Título
+        H2 h2 = new H2("Agregar Solicitud");
+        h2.getStyle().set("text-align", "center");  // Alineamos el título al centro
 
-        FormLayout formulario = new FormLayout();
-        formulario.setWidth("100%");
+        // Formulario
+        FormLayout form = new FormLayout();
+        form.setWidthFull(); // El formulario ocupará todo el ancho disponible
 
+	    //Configuracion de los diferentes componentes del formulario
         titulo.setId("Titulo");
-        intersesados.setId("Intersesados");
-        alimiamiento.setId("Alimiamiento");
-        alcance.setId("Alcance");
-        normativa.setId("Normativa");
-
-
         titulo.setLabel("Titulo");
-        titulo.setPlaceholder("Indique un titulo suficientemente descriptivo");
-        intersesados.setLabel("Intersesados");
-        intersesados.setPlaceholder("Indique el grupo de personas que desean llevar a cabo el proyecto");
-        alimiamiento.setLabel("Alimiamiento");
-        alimiamiento.setPlaceholder("Indique el objetivo del proyecto");
-        alcance.setLabel("Alcance");
-        alcance.setPlaceholder("Indique el grupo de personas a las que beneficiará el proyecto");
-        normativa.setLabel("Normativa");
-        normativa.setPlaceholder("Indique el codigo de la normativa de aplicación");
 
+        nombre.setId("Nombre");
+        nombre.setLabel("Nombre Corto del Proyecto");
+
+        interesados.setId("Interesados");
+        interesados.setLabel("Interesados del Proyecto");
+
+        objetivos.setId("Objetivos");
+        objetivos.setLabel("Objetivos del Proyecto");
+
+        alcance.setId("Alcance");
+        alcance.setLabel("Alcance");
+
+        normativa.setLabel("Normativa");
+        normativa.setPlaceholder("Seleccione Normativa:");
+        //funcion para todos los roles
+
+        avalador.setLabel("Avalador");
+        avalador.setPlaceholder("Seleccione Avalador:");
+        //funcion que tenga todos los avaladores que hay por su rol
+
+
+        fecha_puesta.setLabel("Fecha");
+        fecha_puesta.setPlaceholder("Seleccione Fecha");
+        fecha_puesta.setAutoOpen(false);
 
         Upload upload = new Upload(memoria);
         upload.setAcceptedFileTypes("application/pdf");
@@ -77,15 +104,50 @@ public class SolicitudAddView extends Composite<VerticalLayout> {
             }
         });
 
-        formulario.add(titulo, intersesados, alimiamiento, normativa, submitButton);
-        getContent().add(formulario);
+
+        // Componentes del formulario
+        form.add(titulo, nombre, interesados, objetivos, alcance, normativa, avalador, fecha_puesta,normativa, submitButton);
+
+        form.setColspan(submitButton,2);
+
+        // Ajustar la cantidad de columnas para que se vea bien en diferentes tamaños
+        form.setResponsiveSteps(
+                new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep("550px", 2),
+                new FormLayout.ResponsiveStep("800px", 4)
+        );
+
+        //Guardar
+        Button guardar = new Button("Guardar", e -> add());
+        guardar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        HorizontalLayout guardarLayout = new HorizontalLayout();
+        guardarLayout.setWidth("min-content");
+        guardarLayout.add(guardar);
+        guardarLayout.setAlignItems(FlexComponent.Alignment.CENTER);
 
 
+        // Botón "Volver"
+        Button volver = new Button("Volver");
+        volver.setWidth("min-content");
+        volver.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        volver.addClickListener(e -> UI.getCurrent().navigate("/solicitudes"));
 
+        // Layout para el footer con el botón
+        HorizontalLayout footer = new HorizontalLayout(volver);
+        footer.setWidthFull();
+        footer.add(volver);
+        footer.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
 
+        // Agregar el formulario y el botón al layout principal
+        principal.add(h2, form);
 
-
-
-
+        // Agregar el layout principal al contenido
+        getContent().add(principal,guardar,footer);
     }
+
+    private void add(){}
+
+
+
 }
