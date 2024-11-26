4b705de73b38dfc2616cd2717bfea68caa55f0e7
Celia <92980596+celiagg99@users.noreply.github.com>
2024-11-26 16:22:45 +0100

    Navegabilidad

--- carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeAdminVIew.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeAdminVIew.java
@@ -1,6 +1,7 @@
 package es.uca.iw.carteruca.views.home;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
@@ -10,18 +11,30 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import es.uca.iw.carteruca.views.admin.CentroAllView;
+import es.uca.iw.carteruca.views.admin.UsuarioAllView;
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 
 @Route("/home-admin")
 @PageTitle("Home")
-@RolesAllowed("Admin")
+//@RolesAllowed("Admin")
+@PermitAll
 public class HomeAdminView extends Composite<VerticalLayout> {
 
     public HomeAdminView() {
 
         Div usuario = createSquare("Usuarios", VaadinIcon.USER);
 
+        usuario.addClickListener(e-> UI.getCurrent().navigate(UsuarioAllView.class));
+
         getContent().add(usuario);
+
+        Div centro = createSquare("Centros", VaadinIcon.ACADEMY_CAP);
+
+        centro.addClickListener(e-> UI.getCurrent().navigate(CentroAllView.class));
+
+        getContent().add(centro);
 
     }
 
