4220e6139470386834c5b605c5d0129b7526ffd7
Alejandro Parada <alejandro.paradaespinosa@alum.uca.es>
2024-12-06 17:36:03 +0100

    Update Centro.java
    eliminar centro_sec

--- carteruca/src/main/java/es/uca/iw/carteruca/models/usuario/Centro.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/models/usuario/Centro.java
@@ -9,7 +9,7 @@ import org.checkerframework.common.aliasing.qual.Unique;
 public class Centro {
 
     @Id
-    @GeneratedValue //cambiu9oa estrqtegia segura pero lenta
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
