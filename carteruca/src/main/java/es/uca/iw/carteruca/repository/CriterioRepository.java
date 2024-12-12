fd3aa96a230a5e8dcde89ec8006d86507bbe356c
Celia <92980596+celiagg99@users.noreply.github.com>
2024-12-12 15:16:05 +0100

    Criterio - CRUD

--- carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
@@ -4,4 +4,9 @@ import es.uca.iw.carteruca.models.Criterio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CriterioRepository extends JpaRepository<Criterio, Long> {
+
+    Criterio findByDescripcion(String descripcion);
+
+    void deleteById(Long id);
+
 }
