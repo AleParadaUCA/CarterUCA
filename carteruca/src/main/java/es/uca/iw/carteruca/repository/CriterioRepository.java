bae25cec138642b159445eddd011a02ced45efc1
Alejandro Parada <alejandro.paradaespinosa@alum.uca.es>
2024-12-13 17:16:10 +0100

    Revert "Merge branch 'Ivan'"
    This reverts commit d0c5a6a30f1770af36a0c232c670502606b143ca, reversing
    changes made to 50f47509ae4f2721231e086f283ebdc0ef3e84a7.

--- carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
@@ -2,13 +2,10 @@ package es.uca.iw.carteruca.repository;
 
 import es.uca.iw.carteruca.models.Criterio;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface CriterioRepository extends JpaRepository<Criterio, Long> {
 
     Criterio findByDescripcion(String descripcion);
-
-    List<Float> findAllPeso();//Devuelve el peso dado un id (esto servirá para el calculo de puntuación)
 
     void deleteById(Long id);
 
--- carteruca/src/main/java/es/uca/iw/carteruca/repository/ProyectoRepository.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/repository/ProyectoRepository.java
@@ -2,9 +2,6 @@ package es.uca.iw.carteruca.repository;
 
 import es.uca.iw.carteruca.models.Proyecto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {
-
-    List<Proyecto> findProyectoBy();
 }
