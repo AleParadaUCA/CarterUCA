41501cc51da99d76e7232663052df1a0da7dd59f
Danivangel <ivanguillen66@gmail.com>
2024-12-19 03:24:21 +0100

    Creado funcion para las puntuaciones

--- carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
@@ -2,11 +2,17 @@ package es.uca.iw.carteruca.repository;
 
 import es.uca.iw.carteruca.models.Criterio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CriterioRepository extends JpaRepository<Criterio, Long> {
 
     Criterio findByDescripcion(String descripcion);
 
     void deleteById(Long id);
+
+    @Query("select c.peso from Criterio c")
+    List<Float> findAllPesos();
 
 }
--- carteruca/src/main/java/es/uca/iw/carteruca/services/ProyectoService.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/services/ProyectoService.java
@@ -11,26 +11,26 @@ import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import es.uca.iw.carteruca.models.Proyecto;
 import es.uca.iw.carteruca.models.Solicitud;
 import es.uca.iw.carteruca.repository.ProyectoRepository;
+import es.uca.iw.carteruca.repository.CriterioRepository;
 
 @Service
 public class ProyectoService {
 
     private final ProyectoRepository repository;
-    //private final SolicitudService solicitudService;
+    private final CriterioRepository criterioRepository;
 
     @Autowired
-    public ProyectoService(ProyectoRepository repository) {
+    public ProyectoService(ProyectoRepository repository, CriterioRepository criterioRepository) {
         this.repository = repository;
-        //this.solicitudService = solicitudService;
+        this.criterioRepository = criterioRepository;
     }
 
-    public void guardarProyecto(Float puntuacion, Float porcentaje, MultiFileMemoryBuffer buffer, Solicitud solicitud) {
+    public void guardarProyecto( Float porcentaje, MultiFileMemoryBuffer buffer, Solicitud solicitud) {
 
         List<String> documento = CommonService.guardarFile(buffer, "../archivos"+solicitud.getCartera().getNombre()+"/proyectos");
 
         Proyecto proyecto = new Proyecto();
 
-        proyecto.setPuntuacion(puntuacion);
         proyecto.setPorcentaje(porcentaje);
         proyecto.setEspecificacion_tecnica(documento.get(0));
         proyecto.setPresupuesto(documento.get(1));
@@ -46,5 +46,22 @@ public class ProyectoService {
     public List<Proyecto> getProyectosFinalizadosPorCartera(Long carteraId) {
         // Filtra los proyectos por carteraId y estado "FINALIZADO"
         return repository.findBySolicitud_Cartera_IdAndSolicitud_Estado(carteraId, Estado.ACEPTADO);
+    }
+
+    public void puntuacion(List<Float> vector, Proyecto proyecto) {  //
+        List<Float> pesos = criterioRepository.findAllPesos();
+        Float total = 0.0f;
+
+        if (pesos.size() != vector.size()) {
+            throw new IllegalArgumentException("Error, no coincide la cantidad de criterios con las puntuaciones dadas");
+        }
+
+        for (int i = 0; i < pesos.size(); i++) {
+            total += pesos.get(i) * vector.get(i);
+        }
+
+        proyecto.setPuntuacion(total);
+        repository.save(proyecto);
+
     }
 }
