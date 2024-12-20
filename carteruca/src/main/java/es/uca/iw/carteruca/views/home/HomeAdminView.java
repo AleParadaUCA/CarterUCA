ff16eceea8013afd23bc22491557885d189e2df2
Celia <92980596+celiagg99@users.noreply.github.com>
2024-12-20 22:58:26 +0100

    Recolocación - HomeAdminView
    Recolocación de elementos en vista HomeAdminView

--- carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeAdminView.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/views/home/HomeAdminView.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -18,34 +19,34 @@ import jakarta.annotation.security.RolesAllowed;
 @Route("/home-admin")
 @PageTitle("Home")
 @RolesAllowed("Admin")
-
 public class HomeAdminView extends Composite<VerticalLayout> {
 
     public HomeAdminView() {
 
+        HorizontalLayout primera_fila = new HorizontalLayout();
+        HorizontalLayout segunda_fila = new HorizontalLayout();
+
+        // Primera fila
         Div usuario = common.createSquare("Usuarios", VaadinIcon.USER);
-
-        usuario.addClickListener(e-> UI.getCurrent().navigate(UsuarioAllView.class));
-
-        getContent().add(usuario);
+        usuario.addClickListener(e -> UI.getCurrent().navigate(UsuarioAllView.class));
 
         Div centro = common.createSquare("Centros", VaadinIcon.ACADEMY_CAP);
+        centro.addClickListener(e -> UI.getCurrent().navigate(CentroAllView.class));
 
-        centro.addClickListener(e-> UI.getCurrent().navigate(CentroAllView.class));
+        primera_fila.add(usuario, centro);
+        primera_fila.setSpacing(true); // Espaciado entre columnas
 
-        getContent().add(centro);
-
+        // Segunda fila
         Div cartera = common.createSquare("Cartera", VaadinIcon.CLIPBOARD);
-
         cartera.addClickListener(e -> UI.getCurrent().navigate(CarteraAllView.class));
 
-        getContent().add(cartera);
-
         Div criterio = common.createSquare("Criterio", VaadinIcon.CHECK_CIRCLE);
-
         criterio.addClickListener(e -> UI.getCurrent().navigate(CriterioAllView.class));
 
-        getContent().add(criterio);
-    }
+        segunda_fila.add(cartera, criterio);
+        segunda_fila.setSpacing(true); // Espaciado entre columnas
 
+        // Añadir filas al layout principal
+        getContent().add(primera_fila, segunda_fila);
+    }
 }
