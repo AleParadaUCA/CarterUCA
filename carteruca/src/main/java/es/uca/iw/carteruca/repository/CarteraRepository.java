cd668efa3fb64fdee46f7c99b0c8977efe1d9a5a
Celia <92980596+celiagg99@users.noreply.github.com>
2024-12-31 15:58:32 +0100

    Repository

--- carteruca/src/main/java/es/uca/iw/carteruca/repository/CarteraRepository.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/repository/CarteraRepository.java
@@ -2,8 +2,6 @@ package es.uca.iw.carteruca.repository;
 
 import es.uca.iw.carteruca.models.Cartera;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
--- carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
+++ carteruca/src/main/java/es/uca/iw/carteruca/repository/CriterioRepository.java
@@ -2,9 +2,7 @@ package es.uca.iw.carteruca.repository;
 
 import es.uca.iw.carteruca.models.Criterio;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 
 public interface CriterioRepository extends JpaRepository<Criterio, Long> {
 
@@ -12,7 +10,5 @@ public interface CriterioRepository extends JpaRepository<Criterio, Long> {
 
     void deleteById(Long id);
 
-    @Query("select c.peso from Criterio c")
-    List<Float> findAllPesos();
 
 }
