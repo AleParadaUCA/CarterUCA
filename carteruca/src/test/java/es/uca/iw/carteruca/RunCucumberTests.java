c74ffb7e633158ea58f19b4bd1b09082b60a3b87
Alejandro Parada <alejandro.paradaespinosa@alum.uca.es>
2025-01-02 12:36:27 +0100

    Cucumber start

---
+++ carteruca/src/test/java/es/uca/iw/carteruca/RunCucumberTests.java
@@ -0,0 +1,12 @@
+package es.uca.iw.carteruca;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+
+@IncludeEngines("cucumber")
+@SelectClasspathResource("es/uca/iw/carteruca/")
+public class RunCucumberTests {
+
+}
+
+    
